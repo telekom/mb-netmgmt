83b02e054f23ea485e26f43966a9cc98e69c7f99
Christian Sahlmann <christian.sahlmann@telekom.de>
2022-06-09 12:55:54 +0200

    add test for connecting to ssh imposter

--- test/test_mb_netmgmt.py
+++ test/test_mb_netmgmt.py
@@ -1,8 +1,23 @@
+from threading import Thread
+import paramiko
 import pytest
-from mb_netmgmt import mb
+from mb_netmgmt import mb, ssh
+from mb_netmgmt.__main__ import create_server
 
 
 @pytest.mark.parametrize("protocol", ["http", "snmp", "telnet", "netconf"])
 def test_create_imposter(protocol):
     with mb([{"protocol": protocol, "port": 8080}]):
         pass
+
+
+def test_ssh():
+    ssh.Handler.handle_request = lambda *args: None
+    port = 2222
+    server = create_server(ssh, port, None)
+    Thread(target=server.serve_forever).start()
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    client.connect("localhost", port)
+    client.close()
+    server.shutdown()
