93171d803cfb0a53c682b9fd3ed802bf137eaf03
Christian Sahlmann <christian.sahlmann@telekom.de>
2022-06-13 11:32:43 +0200

    fix yaml dependency

--- mb_netmgmt/__init__.py
+++ mb_netmgmt/__init__.py
@@ -18,7 +18,7 @@
 # along with mb-netmgmt. If not, see <https://www.gnu.org/licenses/
 
 """Network Management Protocols for Mountebank"""
-__version__ = "0.0.12"
+__version__ = "0.0.13"
 
 import os
 import subprocess
