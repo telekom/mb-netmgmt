d290b6ca3a7fcaa71fc0d36fea97fb669d601052
Gabor <82544733+phoenixszg@users.noreply.github.com>
2023-06-22 11:08:33 +0200

    Black fromatting (#4)
    * update telnet.py
    
    * update telnet.py
    
    * update ssh.py
    
    * update telnet.py
    
    * update telnet.py
    
    * update telnet.py
    
    * update telnet.py
    
    * update telnet.py
    
    * match for initial command as well
    
    * define cli patterns centrally
    
    * comment
    
    * black
    
    ---------

--- mb_netmgmt/__main__.py
+++ mb_netmgmt/__main__.py
@@ -122,11 +122,18 @@ class Protocol:
 
     def get_cli_patterns(self):
         patterns = []
-        patterns.append(b"[\r\n\x1b\[K][\-\w+\.:/]+(?:\([^\)]+\))?[>#] ?$")  # based on IOS driver of Exscript
-        patterns.append(b"[\r\n\x00\x1b\[K]RP/\d+/(?:RS?P)?\d+\/CPU\d+:[^#]+(?:\([^\)]+\))?#$")  # based on IOS XR driver of Exscript
-        patterns.append(b"[\r\n\x00\x1b\[K](?P<text>[\w/ .:,\(\)\-\?]*)(?P<default>\[[\w/.:\-]*\])?(?(default)(?P<end1>(?:\?|: ?|)$)|(?P<end2>: $))")  # Interactive prompt
+        patterns.append(
+            b"[\r\n\x1b\[K][\-\w+\.:/]+(?:\([^\)]+\))?[>#] ?$"
+        )  # based on IOS driver of Exscript
+        patterns.append(
+            b"[\r\n\x00\x1b\[K]RP/\d+/(?:RS?P)?\d+\/CPU\d+:[^#]+(?:\([^\)]+\))?#$"
+        )  # based on IOS XR driver of Exscript
+        patterns.append(
+            b"[\r\n\x00\x1b\[K](?P<text>[\w/ .:,\(\)\-\?]*)(?P<default>\[[\w/.:\-]*\])?(?(default)(?P<end1>(?:\?|: ?|)$)|(?P<end2>: $))"
+        )  # Interactive prompt
         patterns.append(b"[\r\n\x00\x1b\[K] --More-- $")  # Terminal paging
         return patterns
+
 
 def disable_algorithms(disabled_algorithms):
     # https://github.com/ncclient/ncclient/issues/526#issuecomment-1096563028
